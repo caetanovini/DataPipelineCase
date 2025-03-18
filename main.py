e4c4a7608ffb80dd8877a8e93d4e97c0ec004bb1
caetanovini <viniciusnandez@gmail.com>
2025-03-18 02:24:14 +0000

    feat: fix main.py file

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 import libs.install_dependencies as install_dependencies
-import data_architecture.codes.bronze_layer as bronze_layer 
-import data_architecture.codes.silver_layer as silver_layer
-import data_architecture.codes.golden_layer as golden_layer
-from libs.spark_session_manager import SparkSessionManager
+from setup.airflow import airflow_setup  # Import the Airflow setup function we created
+import os
+import subprocess
+import sys
+
+def trigger_airflow_dag(dag_id):
+    print(f"Triggering the Airflow DAG '{dag_id}'...")
+
+    try:
+        # Trigger the specified DAG (dag_id) using Airflow CLI
+        subprocess.check_call(['airflow', 'dags', 'trigger', dag_id])
+        print(f"DAG '{dag_id}' triggered successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error triggering the DAG '{dag_id}': {e}")
+        sys.exit(1)
+
+def main():
+    try:
+        # Airflow setup
+        print("Setting up Airflow...")
+        airflow_setup.setup_airflow()  # This will handle Airflow setup, starting scheduler and webserver
+        print('Airflow setup was successful')
+
+        # Trigger the Airflow DAG 'data_pipeline_dag'
+        trigger_airflow_dag('data_pipeline_dag')
+
+    except Exception as e:
+        print(f"Error in main process: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
-    # Fetch the data from the bronze layer
-    # bronze_layer.fetch_brewery_data()
-
-    jdbc_driver_path = "libs/sqlite-jdbc-3.36.0.3.jar"
-
-    # Create a Spark session
-    spark_manager = SparkSessionManager(driver_path=jdbc_driver_path)
-    spark = spark_manager.get_spark_session()
-    
-    try:
-        # Run the silver layer transformation
-        silver_layer.transform_to_silver_layer(spark)
-        
-        # Run the golden layer transformation
-        golden_layer.transform_to_gold_layer(spark)
-
-        # Save golden layer data into a SQLite Database
-        jdbc_url = "jdbc:sqlite:data_architecture/data_sources/brewery_data.db"  # Path to your SQLite database file
-        table_name = "golden_layer"  # Name of the table where the data was saved
-
-        # Load data from the SQLite table into a Spark DataFrame
-        df = spark.read \
-            .format("jdbc") \
-            .option("url", jdbc_url) \
-            .option("dbtable", table_name) \
-            .load()
-
-        # Show the DataFrame to verify the data
-        df.show(10)
-
-    finally:
-        # Stop the Spark session after processing is complete
-        spark_manager.stop_spark_session()
-
+    main()
